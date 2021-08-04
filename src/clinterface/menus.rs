495025918601b08a51bd8b92e90ce53d3e918b59
MarijnBuitenweg <marijnbuitenweg@gmail.com>
2021-08-04 02:21:16 +0200

    Add menus for future features

--- src/clinterface/menus.rs
+++ src/clinterface/menus.rs
@@ -1,10 +1,25 @@
 //This file contains all the information needed to construct the menus
 use super::core::Menu;
 
-pub const MENU_ARRAY: [Menu; 1] = [
+pub const MENU_ARRAY: [Menu; 4] = [
     Menu {
         prompt: "Welcome to Doice, a simple dice rolling program.",
-        items: &["Dice console", "Load character", "Exit"],
-        mapping: &[-1, -2, isize::MAX],
+        items: &["Dice console", "Load character", "Combat", "Character Creation", "Exit"],
+        mapping: &[-1, -2, 1, 2, isize::MAX],
     },
+    Menu {
+        prompt: "Choose a combat-related category:",
+        items: &["Special", "Back"],
+        mapping: &[3, 0],
+    },
+    Menu {
+        prompt: "Here is a set of character creation tools:",
+        items: &["Health roller", "Back"],
+        mapping: &[-4, 0],
+    },
+    Menu {
+        prompt: "Special abilities that may or may not be tied to any specific character:",
+        items: &["Starsword bombardment", "Back"],
+        mapping: &[-3, 0]
+    }
 ];
