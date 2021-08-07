a5a5c418ad96eb1630e589aaac363f15f74470ab
Marijn Buitenweg <marijnbuitenweg@gmail.com>
2021-08-07 19:27:06 +0200

    Update version number
    Add version number to main menu

--- src/clinterface/menus.rs
+++ src/clinterface/menus.rs
@@ -1,9 +1,11 @@
+use std::fmt;
+
 //This file contains all the information needed to construct the menus
 use super::core::Menu;
 
 pub const MENU_ARRAY: [Menu; 4] = [
     Menu {
-        prompt: "Welcome to Doice, a simple dice rolling program.",
+        prompt: &concat!("Welcome to Doice ", env!("CARGO_PKG_VERSION"), ", a simple dice rolling program."),
         items: &["Dice console", "Load character", "Combat", "Character Creation", "Exit"],
         mapping: &[-1, -2, 1, 2, isize::MAX],
     },
