60815ba585977c0e03245be2aabebc11145152dc
Marijn Buitenweg <marijnbuitenweg@gmail.com>
2022-11-14 23:27:32 +0100

    put doice help in a box

--- doice_lib/doice_gui/src/components/dice_docs.rs
+++ doice_lib/doice_gui/src/components/dice_docs.rs
@@ -2,7 +2,11 @@ use doice_roller::FUNCTION_DOCS;
 use eframe::egui::Ui;
 
 pub fn dice_docs(ui: &mut Ui) {
-    for (name, doc) in FUNCTION_DOCS.iter() {
-        ui.collapsing(*name, |ui| ui.label(*doc));
-    }
+    ui.group(|ui| {
+        ui.set_height(ui.available_width());
+        ui.set_width(ui.available_width());
+        for (name, doc) in FUNCTION_DOCS.iter() {
+            ui.collapsing(*name, |ui| ui.label(*doc));
+        }
+    });
 }
