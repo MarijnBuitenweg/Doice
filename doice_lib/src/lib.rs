c9b4cdba29e3cc1449d8218835e671a0423c3938
Marijn Buitenweg <marijnbuitenweg@gmail.com>
2023-02-24 21:38:36 +0100

    Gitui test commit

--- doice_lib/src/lib.rs
+++ doice_lib/src/lib.rs
@@ -1,4 +1,5 @@
 /// Contains all activities
 pub mod activities;
-/// Contains UI setup tailored for ease of use
+/// Contains UI setup tailored for ease of use.
+/// It is here instead of in doice_gui, because it is not component agnostic like the other ui layouts (and as such depends on the specific ui components).
 mod tailored_frames;
